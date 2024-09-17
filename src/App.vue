cb6e1a79b50c44467fcac45605d90199d2be5329
Johnni5 <johnni5@proton.me>
2024-09-17 18:13:11 +0200

    "created first component"

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,41 @@
 <template>
-  <div id="app">
-  </div>
+
+    <Card>
+      <h1>Mein Titel</h1>
+      <p>Ein paar Details hier.</p>
+    </Card>
+
 </template>
 
 <script>
-
+import Card from './components/Card.vue';
 
 export default {
+
   name: 'stock-Dashboard',
   components: {
- 
+    Card,
   }
 }
 </script>
 
 <style>
   body {
-  margin: 0;
-    padding: 0; 
+    margin: 0;
+    /* padding: 0;  */
   }
 
   #app {
+    width: 100vw;
+    min-height: 100vh;
+    padding: 100px;
     background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%20%);
-
+    box-sizing: border-box;
     font-family: 'Rubik', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     /* margin-top: 60px; */
-
-    width: 100vw;
-    min-height: 100vh;
   }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card-component',
+};
+</script>
+
+<style scoped>
+  .card {
+    width: 90%;
+    background-color: #001f35;
+    border-radius: 16px;
+    padding: 24px 32px;
+  }
+</style>
